a767a4620f31fc0ebd19f592b91dc8c1cc347ffc
lucasestevaos <lucas.estevao@hotmail.com.br>
2021-06-14 23:53:18 -0300

    trabalhando mais o front e fazendo a ligacao com a Binance por meio de API

--- force-app/main/default/aura/TradeCmp/TradeCmpController.js
+++ force-app/main/default/aura/TradeCmp/TradeCmpController.js
@@ -1,5 +1,24 @@
 ({
-    myAction : function(component, event, helper) {
+    handleOrder : function(component, event, helper) {
+        var action = component.get("c.getPrice");
 
+        action.setParams({
+            "mercadoId" : component.get("v.recordId")
+        })
+        action.setCallback(this, function(response){
+            var state = response.getState();
+            if(state == "SUCCESS"){
+                //console.log(response.getReturnValue());
+                alert('O preço atual é : ' +  response.getReturnValue());
+            }else{
+                console.log("Erro :" + JSON.stringify(response.getError()));
+            }
+        })
+
+        $A.enqueueAction(action);
+    },
+    handleChange : function(component, event, helper) {
+        //console.log(event.getParam("checked"));
+        component.set("v.opMercado", !event.getParam("checked"));
     }
 })
